2a41e3f263dfc7401df7ad6e0dde912f5bec6cfa
AKRAM2002-coder <120340776+AKRAM2002-coder@users.noreply.github.com>
2023-07-26 00:16:52 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import About from './components/About';
+import Skills from './components/Skills';
+import Work from './components/Work';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Work/>
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
